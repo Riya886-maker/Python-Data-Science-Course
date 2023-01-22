519d15ab20b6c9198f3a13a15eb0141e7b6a7084
Riya Srivastava <xyz@gmail.com>
2023-01-22 18:31:56 +0530

    dict#1

---
+++ contacts.py
@@ -0,0 +1,29 @@
+from pprint import pp
+contacts={
+    'emergency':112}
+while True:
+    print('1.Add Contact')
+    print('2.View Contacts')
+    print('3.Delete contact')
+    print('4.Exit')
+    print("Select a number")
+    ch=int(input('Enter your choice:'))
+    if ch==1:
+        name=input('Enter Name:')
+        number=input('Enter number:')
+        contacts[name]=number
+        print("contact saved")
+    elif ch==2:
+        pp(contacts,width=1)
+    elif ch==3:
+        name=input('Enter Name:')
+        if name in contacts:
+            contacts.pop(name)
+            print("contact deleted")
+        elif ch==4:
+            break
+        else:
+            print("Invalid Choice")
+        
+        
+        
